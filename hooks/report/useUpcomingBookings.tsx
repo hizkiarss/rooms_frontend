7205386394f5f5845d9d72b36e3b08971127b9a1
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-10-01 23:12:41 +0700

    feat(report) add: token for useUpcomingBookings

--- hooks/report/useUpcomingBookings.tsx
+++ hooks/report/useUpcomingBookings.tsx
@@ -1,13 +1,19 @@
 import { BookingsType } from "@/types/bookings/BookingsType";
 import { useQuery } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
 import { graphqlClient } from "../graphQL/graphqlClient";
 import { UPCOMING_BOOKINGS_BY_PROPERTY_ID } from "../graphQL/queries";
 
 export const useUpcomingBookings = (propertyId: string) => {
+  const { data: session } = useSession();
   return useQuery<BookingsType[] | null>({
     queryKey: ["Upcoming-bookings", "Property", propertyId],
     queryFn: async () => {
       try {
+        const token = session?.accessToken;
+        graphqlClient.setHeaders({
+          Authorization: `Bearer ${token}`,
+        });
         const response = await graphqlClient.request(
           UPCOMING_BOOKINGS_BY_PROPERTY_ID,
           { propertyId }
