30167453fb53b81feaf6abde96628c10d7811048
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-10-06 22:47:30 +0700

    feat(my-order) remove: unused code

--- app/my-order/component/OrderList.tsx
+++ app/my-order/component/OrderList.tsx
@@ -10,71 +10,7 @@ import { Separator } from "@radix-ui/react-select";
 import React, { useState } from "react";
 import OrderListItem from "./OrderListItem";
 
-// const OrderList = () => {
-//   const {
-//     data: transactions,
-//     error,
-//     isLoading,
-//     refetch,
-//   } = useTransactionsByUsersId();
-
-//   if (isLoading) {
-//     return <LoadingStateAnimation />;
-//   }
-
-//   if (error) {
-//     return <ErrorAnimation />;
-//   }
-
-//   const refreshTransactions = () => {
-//     refetch();
-//   };
-
-//   return (
-//     <div className="">
-//       {transactions && (
-//         <Card className="">
-//           <CardHeader>
-//             <CardTitle>My Order</CardTitle>
-//           </CardHeader>
-//           <Separator className="mb-2" />
-//           <CardContent className="space-y-4">
-//             {transactions && transactions.length > 0
-//               ? transactions.map((transaction) => (
-//                   <div key={transaction.id}>
-//                     <OrderListItem
-//                       bookingCode={transaction.bookingCode}
-//                       imgUrl={"/img"}
-//                       totalPrice={transaction.finalPrice}
-//                       propertyName={transaction.properties.name}
-//                       status={transaction.status}
-//                       paymentMethod={transaction.paymentMethod}
-//                       transactionId={transaction.id}
-//                       paymentProofs={transaction.paymentProofs}
-//                       onRefresh={refreshTransactions}
-//                       transactionDetails={transaction.transactionDetails[0]}
-//                       review={transaction.reviews}
-//                       room={transaction.transactionDetails[0].rooms}
-//                     />
-//                   </div>
-//                 ))
-//               : transactions &&
-//                 transactions.length === 0 && (
-//                   <EmptyDataAnimation
-//                     message="No transactions so far, but good things are coming!"
-//                     width={200}
-//                     height={200}
-//                   />
-//                 )}
-//           </CardContent>
-//         </Card>
-//       )}
-//     </div>
-//   );
-// };
-
 const OrderList: React.FC = () => {
-  // const [page, setPage] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
   const [sort, setSort] = useState<string | null>(null);
   const [status, setStatus] = useState<string | null>(null);
@@ -88,13 +24,13 @@ const OrderList: React.FC = () => {
   } = useTransactionsByUsersId(currentPage, pageSize, sort, status);
 
   const handleSortChange = (value: string | null) => {
-    setSort(value || null); // Jika string kosong, set ke null
+    setSort(value || null);
     refetch();
   };
 
   const handleStatusFilterChange = (status: string | null) => {
     setStatus(status || null);
-    refetch(); // Refresh data after status change
+    refetch();
   };
 
   if (isLoading) {
@@ -110,9 +46,8 @@ const OrderList: React.FC = () => {
   };
 
   const handlePageChange = (page: number) => {
-    // Sesuaikan page yang dikirim ke backend (backend mulai dari 0)
     if (transactionPage && page > 0 && page <= transactionPage.totalPages) {
-      setCurrentPage(page - 1); // Mengurangi 1 agar cocok dengan backend
+      setCurrentPage(page - 1);
     }
   };
 
