dbde653756ccc2b0e501e4a993b5c3d5aaa543a1
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-10-22 16:29:43 +0700

    feat(payment-confirmation) remove: unused code

--- app/dashboard/payment-confirmation/page.tsx
+++ app/dashboard/payment-confirmation/page.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import MyForm from "./component/UploadPaymentProofForm";
 import PaymentVerificationList from "./component/PaymentVerificationList";
-import { PaymentVerificationTable } from "./component/PaymentVerificationTable";
 
 const page = () => {
   return (
-    // <div className="px-5 sm:px-10 md:px-20 lg:px-[100px]">
     <div className="p-4">
-      {/* <PaymentVerificationTable /> */}
       <PaymentVerificationList />
-      {/* <MyForm transactionId="41" /> */}
     </div>
   );
 };
